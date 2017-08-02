b9c52ed3f87d38c84414df87ed59905622efdb6a
Andrew Chilton <andychilton@gmail.com>
2017-08-02 17:33:15 +1200

    Add the ability to render pages
    * on the index page
    * on the thanks page
    * standalone

--- schema/patch_4_5.sql
+++ schema/patch_4_5.sql
@@ -5,7 +5,7 @@ CREATE TABLE page (
     id              INTEGER NOT NULL DEFAULT nextval('object_id_seq'::TEXT) PRIMARY KEY,
     name            TEXT UNIQUE NOT NULL, -- e.g. "index", "about", "help", "contact"
     title           TEXT NOT NULL,
-    markdown        TEXT NOT NULL DEFAULT '',
+    content         TEXT NOT NULL DEFAULT '',
     html            TEXT NOT NULL DEFAULT '',
     position        INTEGER UNIQUE, -- 0 = not in menu, n = in that order
 
@@ -19,9 +19,3 @@ INSERT INTO page(name, title) VALUES('index', 'Index');
 INSERT INTO page(name, title) VALUES('thanks', 'Thanks');
 
 -- ----------------------------------------------------------------------------
-
-
-
-
-INSERT INTO page(name, title, position) VALUES('about', 'About', 1);
-INSERT INTO page(name, title, position) VALUES('contact', 'Contact', 2);
