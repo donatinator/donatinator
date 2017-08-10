339244f471db4f6334f301a3545918ff9ade5433
Andrew Chilton <andychilton@gmail.com>
2017-08-11 10:30:42 +1200

    Add subscription backend - customer/subscription creation on Stripe

--- public/s/js/index.js
+++ public/s/js/index.js
@@ -4,16 +4,15 @@
 
 $(function() {
   // enable the buttons now that everything has loaded
-  $('.js-single').prop('disabled', false);
+  $('.js-donate').prop('disabled', false);
+  $('.js-subscribe').prop('disabled', false);
+  var $form = $('#js-form');
 
   var handler = StripeCheckout.configure({
     image   : 'https://stripe.com/img/documentation/checkout/marketplace.png',
     locale  : 'auto',
     zipCode : true,
     token   : function(token) {
-      // get hold of the hidden "Single Donation" form on the page
-      var $form = $('#js-form-single');
-
       // then, find all the hidden inputs we want to send (if not there, then we can't populate it)
       $form.find("input[name='token_id']").first().val(token.id)
       $form.find("input[name='type']").first().val(token.type)
@@ -25,22 +24,19 @@ $(function() {
     }
   })
 
-  // Listen to clicks on the donate buttons so we can do a custom integration.
-  //
-  // Ref : https://stripe.com/docs/checkout#integration-custom
-  $('.js-single').on('click', function(ev) {
-    ev.preventDefault();
-    var $el = $(this);
-
+  function complete($el) {
     // disable the button for a few seconds so the user can't click it again
     $el.prop('disabled', true);
     setTimeout(function() {
       $el.prop('disabled', false);
     }, 3000);
 
-    // set the amount in the form - since the token doesn't give this back to us later
-    var $form = $('#js-form-single');
+    // get the form and set it's action to either `/donate` or `/subscribe`
+    $form.attr('action', $el.data('action'))
+
+    // set the amount/plan in the form - since the token doesn't give this back to us later
     $form.find("input[name='amount']").first().val($el.data('amount'))
+    $form.find("input[name='plan']").first().val($el.data('plan'))
 
     // Open Checkout with further options:
     handler.open({
@@ -52,7 +48,23 @@ $(function() {
       allowRememberMe : $el.data('allow-remember-me'),
       key             : $el.data('key'),
     });
+  }
 
+  // Listen to clicks on the donate buttons so we can do a custom integration.
+  //
+  // Ref : https://stripe.com/docs/checkout#integration-custom
+  $('.js-donate, .js-subscribe').on('click', function(ev) {
+    ev.preventDefault();
+
+    // complete the Stripe popup
+    var $el = $(this);
+    complete($el);
+  });
+
+  // when the window unloads, close the handler
+  $(window).on('unload', function() {
+    console.log('unload');
+    handler.close();
   });
 
 });
